43580d2313f0a3140b3092a1ca1a0e9988a437ac
tyr <tyr0v.k@yandex.ru>
2025-04-15 19:34:22 +0300

    Lab 2, 5 - проверка длины числа.

--- JS/js_lr2/5.js
+++ JS/js_lr2/5.js
@@ -1,5 +1,9 @@
 function douToDec(n) {
+  if (n.length !== 8){
+    return "Error"
+  }
   return parseInt(n, 2);
 }
 console.log(douToDec("00001001"));
 console.log(douToDec("11110000"));
+console.log(douToDec("111100001"));
